3805459b28973d49376cad3311db7aa4a142841c
hb140502 <hb130502@outlook.com>
2022-12-08 14:04:28 +0100

    Norbert heeft niks gedaan
    Co-authored-by: Norbert <Nibbit99@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from sklearn.cluster import KMeans
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from matplotlib import pyplot as plt
 from toolbox.clusterPlot import clusterPlot
+import random
+
+SAMPLE_SIZE = 100
+DATASET_SIZE = 122410
 
 def oneHotEncodeMap(data):
     # create One Hot Encoder
@@ -31,8 +35,15 @@ def standardize(data):
     scaler = preprocessing.StandardScaler().fit(X)
     return scaler.transform(X)
 
+def getRowsToSkip(sample_size):
+    sample_size = 100 #desired sample size
+    skiprows = sorted(random.sample(range(1, DATASET_SIZE), DATASET_SIZE - sample_size))
+    return skiprows
+
 if __name__ == '__main__':
-    dataset = read_csv('csgo_round_snapshots.csv', nrows=110, header=0)
+    # read random sample of the data
+    dataset = read_csv('csgo_round_snapshots.csv', nrows=110, header=0, skiprows=getRowsToSkip(100))
+    print(dataset)
 
     # attributes without class label
     X = dataset.drop(columns='round_winner')
@@ -49,19 +60,21 @@ if __name__ == '__main__':
     # standardize data
     X_standardized = standardize(X)
 
-
     # Decision Tree Classifier
     dtc = DecisionTreeClassifier(random_state=0)
     dtc.fit(X, y)
-    #plt.figure(figsize=(20,20))
-    #plot_tree(dtc, node_ids=True)
-    #plt.show()
+
+    plt.figure(figsize=(20,20))
+    plot_tree(dtc, node_ids=True)
+    plt.show()
 
     # K-Means Clustering
     kmeans = KMeans(2, random_state=0)
     kmeans.fit(X_standardized)
+    y_pred = kmeans.predict(X_standardized)
     clusters = kmeans.labels_
     centroids = kmeans.cluster_centers_
-
+    
     clusterPlot(X_standardized, clusters, centroids, y)
+    plt.show()
     
