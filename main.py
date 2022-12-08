f242d687fd0c24ab27cfb9b88432e1acf7236930
hb140502 <hb130502@outlook.com>
2022-12-08 14:24:36 +0100

    added 10-fold cross-validation

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 from matplotlib import pyplot as plt
 from toolbox.clusterPlot import clusterPlot
 import random
+from sklearn.model_selection import StratifiedKFold
 
+# size of sample we want to read from dataset
 SAMPLE_SIZE = 100
+# size of dataset
 DATASET_SIZE = 122410
+# random seed used to reproduce results
+RANDOM_SEED = 0
 
 def oneHotEncodeMap(data):
     # create One Hot Encoder
@@ -36,13 +41,18 @@ def standardize(data):
     return scaler.transform(X)
 
 def getRowsToSkip(sample_size):
-    sample_size = 100 #desired sample size
+    # desired sample size
+    sample_size = 100 
+    
+    # set random seed
+    random.seed(RANDOM_SEED)
+
     skiprows = sorted(random.sample(range(1, DATASET_SIZE), DATASET_SIZE - sample_size))
     return skiprows
 
 if __name__ == '__main__':
     # read random sample of the data
-    dataset = read_csv('csgo_round_snapshots.csv', nrows=110, header=0, skiprows=getRowsToSkip(100))
+    dataset = read_csv('csgo_round_snapshots.csv', header=0, skiprows=getRowsToSkip(100))
     print(dataset)
 
     # attributes without class label
@@ -60,21 +70,31 @@ if __name__ == '__main__':
     # standardize data
     X_standardized = standardize(X)
 
-    # Decision Tree Classifier
-    dtc = DecisionTreeClassifier(random_state=0)
-    dtc.fit(X, y)
+    # Stratified 10-fold
+    ten_fold = StratifiedKFold(n_splits=10, shuffle=True, random_state=RANDOM_SEED)
+    for train, test in ten_fold.split(X, y):
+        X_train, X_test, X_standardized_train, X_standardized_test, y_train, y_test = X[train], X[test], X_standardized[train], X_standardized[test], y[train], y[test]
 
-    plt.figure(figsize=(20,20))
-    plot_tree(dtc, node_ids=True)
-    plt.show()
+        # Decision Tree Classifier
+        dtc = DecisionTreeClassifier(random_state=RANDOM_SEED)
+        dtc.fit(X_train, y_train)
 
-    # K-Means Clustering
-    kmeans = KMeans(2, random_state=0)
-    kmeans.fit(X_standardized)
-    y_pred = kmeans.predict(X_standardized)
-    clusters = kmeans.labels_
-    centroids = kmeans.cluster_centers_
+        """
+        plt.figure(figsize=(20,20))
+        plot_tree(dtc, node_ids=True)
+        plt.show()
+        """
+        
+        # K-Means Clustering
+        kmeans = KMeans(2, random_state=RANDOM_SEED)
+        kmeans.fit(X_standardized_train)
+        # y_pred = kmeans.predict(X_standardized)
+        clusters = kmeans.labels_
+        centroids = kmeans.cluster_centers_
+        
+        """
+        clusterPlot(X_standardized_test, clusters, centroids, y)
+        plt.show()
+        """
     
-    clusterPlot(X_standardized, clusters, centroids, y)
-    plt.show()
     
